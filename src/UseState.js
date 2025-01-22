12c1c535193fcc88569ad462035dc37a683457c3
Isabela <isa31297@gmail.com>
2025-01-22 00:48:20 -0500

    loading effect

--- src/UseState.js
+++ src/UseState.js
@@ -1,11 +1,33 @@
 import React from "react";
 
 function UseState({ name }) {
-    const [error, setError] = React.useState(true);
+    const [error, setError] = React.useState(false);
+    const [loading, setLoading] = React.useState(false);
+
+    React.useEffect(() =>{
+        console.log('funcionando empezando')
+        if (!!loading) {
+            setTimeout(() => {
+                console.log('Validandooo validandooo')
+    
+                setLoading(false);
+    
+                console.log('terminando la validacion')
+            }, 3000);   
+        }
+        console.log('funcionando terminando')
+        
+    }, [loading])
     return (
         <div>
             <h2>ELIMINAR {name}</h2>
             <p>Escribe el codiogo de seguridad</p>
+
+            {loading && (
+                <p>
+                    Cargando...
+                </p>
+            )}
 
             {error && (
                 <p>
@@ -14,7 +36,7 @@ function UseState({ name }) {
             )}
             <input placeholder="codigo de seguridad"/>
             <button
-                onClick={() => setError(!error)}>Comprobar</button>
+                onClick={() => setLoading(true)}>Comprobar</button>
         </div>
     )
 }
